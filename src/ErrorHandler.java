efe8c4e82a7f2e4d3d9e82496392e1fc9d598c02
Christoffer Rödöö <christoffer.rodoo@gmail.com>
2010-04-11 18:02:21 +0200

    Skapade en tidig version av ErrorHandler

---
+++ src/ErrorHandler.java
@@ -0,0 +1,14 @@
+
+public final class ErrorHandler {
+	
+	public ErrorHandler() {}
+	
+	public void printMsg(String msg) {
+		System.out.println(msg);
+	}
+	
+	public void printError(String error) {
+		System.err.println(error);
+	}
+
+}
